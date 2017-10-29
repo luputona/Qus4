fb10cb38513b4b67975ecf6c8e0d69e192a8a3ea
luputona <luputona5@gmail.com>
2017-10-29 23:55:03 +0900

    no message

--- Qus4/main.c
+++ Qus4/main.c
@@ -10,83 +10,257 @@
 묻는 단어는 랜덤 (중복 안되게)
 모든 단어를 맞히면 100점
 */
-
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-void SaveDB(char *engDB, char *korDB)
+void Update1()
 {
-	
-}
+	char arrInput[15] = { 0, };
+	char arrEngDB[20] = { 0, };
+	char arrKorDB[20] = { 0, };
 
-
-void main()
-{
-	char arrEng[20] = { 0, };
-	char arrKor[20] = { 0, };
-	char arrInput[] = { 0, };
-	char arrEngDB[] = { 0, };
-	char arrKorDB[] = { 0, };
-	
-	int nEngDBLen = 1;
-	int nKorDBLen = 1;
-	int i;
-
-	char nQuit = 1;
-	int nDBCount = 0;
+	int i, j;
+	int nDbLength = 0;
+	int nQuit = 1;
 
 	printf("영어 단어와 뜻인 한글 단어를 입력 (ex. Apple 사과) \n");
 	printf("0을 입력하면 입력 종료. \n");
-	while (nQuit != 0)
+	
+	while (1)
 	{
 		printf("영어 단어 입력 (0을 입력하면 종료):");
-		scanf_s("%s", arrEng, sizeof(arrEng));
-
-		//printf("한글 단어 입력 (0을 입력하면 종료):");
-		//scanf_s("%s", arrKor, sizeof(arrKor));
-
-		if (arrEng[0] == 0 || arrEng[0] == 0)
+		scanf_s("%s", arrInput, sizeof(arrInput));
+				
+		if (arrInput[0] == '0')
 		{
-			nQuit = 0;
+			break;
+		}		
+		for ( i = 0; i < sizeof(arrInput);i++)
+		{
+			if (arrInput[i] == '\0')
+			{
+				nDbLength = i;
+			}
+		}
+
+		if (arrEngDB[0] == '\0')
+		{
+			for (i = 0; i < nDbLength; i++)
+			{
+				arrEngDB[i] = arrInput[i];
+			}
+		}
+		else
+		{			
+			int temp = 0;
+			int tempj = 1;
+			for (j = 0; j < sizeof(arrEngDB); j++)
+			{
+				if (arrEngDB[j] == '\0' )
+				{					
+					arrEngDB[tempj+j] = arrInput[temp];
+					temp++;	
+					tempj = 0;
+					if (arrInput[temp] == '\0')
+					{
+						break;
+					}
+				}
+			}								
+		}
+
+		printf("한글 단어 입력 (0을 입력하면 종료):");
+		scanf_s("%s", arrInput, sizeof(arrInput));
+
+		if (arrInput[0] == '0')
+		{
+			break;
+		}
+		for (i = 0; i < sizeof(arrInput); i++)
+		{
+			if (arrInput[i] == '\0')
+			{
+				nDbLength = i;
+			}
+		}
+
+		if (arrKorDB[0] == '\0')
+		{
+			for (i = 0; i < nDbLength; i++)
+			{
+				arrKorDB[i] = arrInput[i];
+			}
+		}
+		else
+		{
+			int temp = 0;
+			for (j = 0; j < sizeof(arrKorDB); j++)
+			{
+				if (arrKorDB[j] == '\0')
+				{
+					arrKorDB[j] = arrInput[temp];
+					temp++;
+					if (arrInput[temp] == '\0')
+					{
+						break;
+					}
+				}
+			}
+		}
+		printf("%s \n", arrEngDB);
+		printf("%s \n", arrKorDB);
+	}
+	printf("입력 종료 \n");
+	
+	printf("문제\n");
+	printf("한글 단어를 물어보면 (ex. 사과?) 영어 단어 입력(ex.Apple)\n");
+	while (1)
+	{
+		printf("%s를 영어로 하면??");
+		scanf_s("");
+			
+	}
+
+	printf("exit");
+
+}
+
+void Update()
+{
+	const int MAX = 20;
+
+	char arrInput[20] = { 0, };
+	char arrEngDB[20][20] = {0,};
+	char arrKorDB[20][20] = {0,};
+
+	int i = 0, j = 0;
+	int nEngCounter = 0;
+	int nEngDbCounter = 0;
+	int nKorCounter = 0;
+	int nKorDbCounter = 0;
+	int nInputLength = 0;
+	int nRandomQ = 0;
+
+	int nScore = 0;
+
+	printf("영어 단어와 뜻인 한글 단어를 입력 (ex. Apple 사과) \n");
+	printf("0을 입력하면 입력 종료. \n");
+		
+	//pEngDB[0] = "apple";
+
+	while (1)
+	{
+		printf("영어 단어 입력 (0을 입력하면 종료):");
+		scanf_s("%s", arrInput, sizeof(arrInput));
+
+		if (arrInput[0] == '0')
+		{
+			break;
+		}
+		
+		while (1)
+		{
+			i++;
+			nInputLength = i;
+			if (arrInput[i] == '\0')
+			{
+				i = 0;
+				break;
+			}
+		}
+		for (j = 0; j< nInputLength; j++)
+		{
+			if (arrInput[j] != '\0')
+			{
+				arrEngDB[nEngCounter][j] = arrInput[j];
+			}			
+		}
+		nEngCounter++;
+		nEngDbCounter++;
+
+		printf("한글 단어 입력 (0을 입력하면 종료):");
+		scanf_s("%s", arrInput, sizeof(arrInput));
+		
+		if (arrInput[0] == '0')
+		{
 			break;
 		}
 
-		while (arrEng[nEngDBLen] != '\0')
+		while (1)
 		{
-			nEngDBLen++;
-		}
-		
-		for (i = 0; i <= nEngDBLen; i++)
-		{
-			if (arrEngDB[nEngDBLen] != '\0')
+			i++;
+			nInputLength = i;
+			if (arrInput[i] == '\0')
 			{
-				arrEngDB[i] = arrEng[i];
+				i = 0;
+				break;
 			}
-			else if (arrEngDB[nEngDBLen] != 0)
+		}
+		for (j = 0; j< nInputLength; j++)
+		{
+			if (arrInput[j] != '\0')
 			{
-				arrEngDB[i + nEngDBLen] = arrEng[i];
-			}			
+				arrKorDB[nKorCounter][j] = arrInput[j];
+			}
+		}
+		nKorCounter++;
+		nKorDbCounter++;
+	}
+	i = 0;
+	j = 0;
+
+
+	printf("문제\n");
+	printf("한글 단어를 물어보면 (ex. 사과?) 영어 단어 입력(ex.Apple)\n");
+	while (1)
+	{
+		srand(time(NULL));
+		int nTemp = rand() % (nEngCounter + 1);
+		printf("%s를 영어로 하면??", arrKorDB[nTemp]);
+		scanf_s("%s",arrInput, sizeof(arrInput));
+		
+		while (1)
+		{
+			i++;
+			nInputLength = i;
+			if (arrInput[i] == '\0')
+			{
+				i = 0;
+				break;
+			}
 		}
 
-		/*while (arrKor[nKorDBLen] != '\0')
+		for (i = 0; i < MAX; i++)
 		{
-			nKorDBLen++;
-		}		
+			for (j = 0; j < nInputLength; j++)
+			{
+				if (arrInput[j] != '\0')
+				{
+					if (arrEngDB[i][j] == arrInput[j])
+					{
+						nScore += 10;
+					}
+				}
+			}			
+		}
 		
-		for (i = 0; i<= nKorDBLen; i++)
-		{
-			if (arrKorDB[0] == 0)
-			{
-				arrKorDB[i] = arrKor[i];
-			}
-			else if (arrKorDB[0] != 0)
-			{
-				arrKorDB[i + nKorDBLen] = arrKor[i];
-			}
-		}		*/
-		printf("%s", arrEngDB);
-		printf("%s", arrKorDB);
-		printf("\n");
+
 	}
-	printf("입력 종료\n");
-	
+
+	for (i = 0; i< MAX; i++)
+	{
+		printf("영단어 : %s \n", arrEngDB[i]);
+		printf("한단어 : %s \n", arrKorDB[i]);
+
+	}
+	printf("입력 종료 \n");
+
+}
+
+void main()
+{
+
+	Update();
+
 }
